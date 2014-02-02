ba8eb4d677ad9242025b56bf764f960cf52a115c
Khalid Sookia <khalidsookia@gmail.com>
2014-02-02 15:01:19 +0100

    Added template
    3 layers --
    
    ::base.html.twig
    AppAppBundle::layout.html.twig -- header and footer
    index.html.twig

---
+++ src/App/AppBundle/Controller/AppController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class AppController extends Controller
+{
+    /**
+     * @Route("/hello/{name}")
+     * @Template()
+     */
+    public function indexAction()
+    {
+        return $this->render('AppAppBundle:App:index.html.twig');
+    }
+}
